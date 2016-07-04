5b76e468ab42cde3a822a974cbef0b5b7319d987
Faraz Rehman <farazrehman1@gmail.com>
2016-07-04 01:35:50 -0400

    Made some changes to cor function. Commented out stuff is really out and funny comments are in

--- Programmjng-R/corr.R
+++ Programmjng-R/corr.R
@@ -1,15 +1,10 @@
 corr <- function (directory, threshold = 0){
     
-    # Get the names of all files in the director
-    #all_files = list.files(path = directory, pattern = "*.csv", all.files = FALSE)
-  
-    #sensor_id = (strtoi(substr(all_files, 0, 3)))
-    #substr(all_files, 0,3)
-    #sensor_id = strtoi(substr(all_files, 0,3))
-    #print (sensor_id)
     rvec = NULL
     rid = NULL
     
+    # Bad logic but only couple of hours to meet the deadline, hence brute forcing
+    # First going to make a list of all sensor-ids that meet the theshold 
     for (k in 1:332)
     {
         # Check which files meet the Threshold
@@ -21,29 +16,29 @@ corr <- function (directory, threshold = 0){
         }
     }
     
-    
-    # We know that all sensors in rid vector pass the threshold
-    # So we can go ahead and do correlation
-    # First lets translate sensor id back to file name
-    # Convert sensor-ids to proper file names with padding 0s
+    # We know have all the sensors in rid vector that meet the threshold
+    # So we can go ahead and do correlation - yay!
+    # BUT First lets translate sensor id back to file name - again!! bad! bad!
     
     rfiles <- paste ("specdata", sep = "/", (paste (formatC(rid, width = 3, format = "d", flag = "0"),sep=".","csv")))
     
-    #print (rfiles)
+    # Tada! Now rfiles has all the files that meet the threshold
     cor_val = NULL
     corr_vec = NULL
+    
+    # For each file in rfiles i.e. those meeting the threshold...
+    #   - Read the CSV file into a data.frame
+    #   - Clean-up the data
+    #   - Apply cor() function to calculate correlation for Sulfate and
+    #       Nitrate pollutants
+    #   - Append the value each correlation into a vector and pray it works :)
     for (n in rfiles)
     {
         cor_frame <- read.csv(n, header=TRUE)
         cln_frame <- na.omit(cor_frame)
         cor_val <- cor(cln_frame[,2], cln_frame[,3])
         corr_vec <- append(corr_vec, cor_val)
-        # sulf <- cln_frame [,2]
-        # nit <- cln_frame [,3]
-        # w_data <- c(sulf, nit)
-        # print (t(w_data))
     }
     corr_vec
- 
 }
     
